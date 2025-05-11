4fd73648b61372a33b1a0f35b74b4208995e8908
Chirag Jain <cjain8060@gmail.com>
2025-05-11 20:24:33 +0530

    feat: 3 new uts for post api/create-event

--- __tests__/create-event.test.ts
+++ __tests__/create-event.test.ts
@@ -5,7 +5,7 @@ const mockCollection = {
   add: jest.fn(),
 };
 
-jest.mock("../src/config/firebase.ts", () => ({
+jest.mock("../src/config/firebase", () => ({
   db: {
     collection: jest.fn(() => mockCollection),
   },
@@ -15,18 +15,66 @@ jest.mock("../src/config/firebase.ts", () => ({
 import request from "supertest";
 import app from "../src/app";
 
-describe("POST /api/events", () => {
+const route = "/api/events";
+
+describe(`POST ${route}`, () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it("returns 400 for invalid datetime", async () => {
-    const res = await request(app).post("/api/events").send({
+    const res = await request(app).post(route).send({
       datetime: "not-a-date",
       duration: 30,
     });
     expect(res.status).toBe(400);
     expect(res.body.message).toBeDefined();
     expect(mockCollection.add).not.toHaveBeenCalled();
+  });
+
+  it("returns 400 when duration is negative", async () => {
+    const res = await request(app).post(route).send({
+      datetime: "2025-05-11T08:30:00",
+      duration: -10,
+    });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toBeDefined();
+  });
+
+  // in env for now start time is defined as 8
+  it("rejects booking before work start time", async () => {
+    const res = await request(app).post(route).send({
+      datetime: "2025-05-11T07:30:00",
+      duration: "60",
+    });
+
+    expect(res.status).toBe(422);
+    expect(res.body.message).toBeDefined();
+  });
+
+  // in env for now start time is defined as 8
+  it("rejects booking after work end time", async () => {
+    const res = await request(app).post(route).send({
+      datetime: "2025-05-11T17:00:00",
+      duration: "30",
+    });
+
+    expect(res.status).toBe(422);
+    expect(res.body.message).toBeDefined();
+  });
+
+  it("creates event when correct values are passed", async () => {
+    // @ts-ignore
+    mockCollection.get.mockResolvedValueOnce({ docs: [] });
+    // @ts-ignore
+    mockCollection.add.mockResolvedValueOnce({ id: "test-id" });
+
+    const res = await request(app).post(route).send({
+      datetime: "2025-05-11T08:30:00",
+      duration: 30,
+    });
+
+    expect(res.status).toBe(200);
   });
 });
