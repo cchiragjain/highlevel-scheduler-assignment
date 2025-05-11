6bb82bb54ca4bc9a4596f4938c367c0fc6773dd9
Chirag Jain <cjain8060@gmail.com>
2025-05-11 20:52:46 +0530

    feat: uts completed for create event

--- __tests__/create-event.test.ts
+++ __tests__/create-event.test.ts
@@ -10,6 +10,10 @@ jest.mock("../src/config/firebase", () => ({
     collection: jest.fn(() => mockCollection),
   },
 }));
+const doc = (payload: any) => ({
+  id: "doc-id",
+  data: () => payload,
+});
 
 // importing after so that the original app does not load
 import request from "supertest";
@@ -65,6 +69,7 @@ describe(`POST ${route}`, () => {
   });
 
   it("creates event when correct values are passed", async () => {
+    // TODO: check if can find types
     // @ts-ignore
     mockCollection.get.mockResolvedValueOnce({ docs: [] });
     // @ts-ignore
@@ -76,5 +81,57 @@ describe(`POST ${route}`, () => {
     });
 
     expect(res.status).toBe(200);
+  });
+
+  it("rejects booking event if event already overlaps", async () => {
+    //@ts-ignore
+    mockCollection.get.mockResolvedValueOnce({
+      docs: [
+        doc({
+          datetime: "2025-05-11T12:30:00.000Z", // 8:30 in us/eastern firebase stores in uct
+          duration: 30,
+        }),
+      ],
+    });
+
+    const res = await request(app).post(route).send({
+      datetime: "2025-05-11T08:30:00",
+      duration: 55,
+    });
+
+    expect(res.status).toBe(422);
+  });
+
+  it("rejects 08:00-08:40 booking because it overlaps 08:30-09:00", async () => {
+    //@ts-ignore
+    mockCollection.get.mockResolvedValueOnce({
+      docs: [
+        doc({
+          datetime: "2025-05-11T12:30:00.000Z", // dummy booking from 8:30 - 9
+          duration: 30,
+        }),
+      ],
+    });
+
+    const res = await request(app).post(route).send({
+      datetime: "2025-05-11T08:00:00",
+      duration: 40,
+    });
+
+    expect(res.status).toBe(422);
+  });
+
+  it("rejects 13:30-14:10 booking because it overlaps 14:00-14:50", async () => {
+    // @ts-ignore
+    mockCollection.get.mockResolvedValueOnce({
+      docs: [doc({ datetime: "2025-05-11T18:00:00.000Z", duration: 50 })],
+    });
+
+    const res = await request(app).post(route).send({
+      datetime: "2025-05-11T13:30:00",
+      duration: 40,
+    });
+
+    expect(res.status).toBe(422);
   });
 });
